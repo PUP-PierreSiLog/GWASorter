ce4e7114c0db52c5bb87ea724ef2c9c85ae61da8
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-26 07:33:04 +0800

    Created an if loop for comparing the GWA

--- GWA_Sorter.py
+++ GWA_Sorter.py
@@ -1,13 +1,15 @@
 #GWA Sorter
 #Creates a variable for the student's name and their corresponding GWAs
-student_name=""
-student_gwa=0
+highest_name=""
+highest_gwa=0
 #Reads a file named 20 Students.txt
 with open("D:\[CMPE103] OOP\GWA_Sorter\\20 Students") as source:
     lines=source.readlines()
 #Since I created a CSV, I can now strip and split the name of the students and their GWAs of commas using the function:
 for line in lines:
     name, gwa=line.strip().split(",")
-    print(name, gwa)
 #If loop comparing the values of the students' GWA
+if gwa > highest_gwa:
+    highest_gwa=gwa
+    highest_name=name
 #Print output
