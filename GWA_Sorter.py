e2bc21cf02b032ccf56c385c31608f257a7c5e69
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-26 07:22:40 +0800

    Created a variable for the student's name and GWA, and opened the source file

--- GWA_Sorter.py
+++ GWA_Sorter.py
@@ -1,6 +1,10 @@
 #GWA Sorter
 #Creates a variable for the student's name and their corresponding GWAs
+student_name=""
+student_gwa=0
 #Reads a file named 20 Students.txt
+with open("D:\[CMPE103] OOP\GWA_Sorter\\20 Students.txt") as source:
+    source.readlines()
 #Since I created a CSV, I can now strip the name of the students and their GWAs of commas using the function:
 #If loop comparing the values of the students' GWA
 #Print output
