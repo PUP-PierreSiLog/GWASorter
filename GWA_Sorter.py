300d2ed29469d54ae65deacce03b7e0ed8db7708
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-26 07:20:56 +0800

    Created basic outline of my program

--- GWA_Sorter.py
+++ GWA_Sorter.py
@@ -1,1 +1,6 @@
 #GWA Sorter
+#Creates a variable for the student's name and their corresponding GWAs
+#Reads a file named 20 Students.txt
+#Since I created a CSV, I can now strip the name of the students and their GWAs of commas using the function:
+#If loop comparing the values of the students' GWA
+#Print output
