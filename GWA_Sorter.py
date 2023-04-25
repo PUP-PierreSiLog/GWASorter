27158a111c73ae2407ee081936fd1491ddc89861
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-26 07:30:40 +0800

    Stripped and split the name and gwa respectively of each person to compare their values with each other

--- GWA_Sorter.py
+++ GWA_Sorter.py
@@ -3,8 +3,11 @@
 student_name=""
 student_gwa=0
 #Reads a file named 20 Students.txt
-with open("D:\[CMPE103] OOP\GWA_Sorter\\20 Students.txt") as source:
-    source.readlines()
-#Since I created a CSV, I can now strip the name of the students and their GWAs of commas using the function:
+with open("D:\[CMPE103] OOP\GWA_Sorter\\20 Students") as source:
+    lines=source.readlines()
+#Since I created a CSV, I can now strip and split the name of the students and their GWAs of commas using the function:
+for line in lines:
+    name, gwa=line.strip().split(",")
+    print(name, gwa)
 #If loop comparing the values of the students' GWA
 #Print output
