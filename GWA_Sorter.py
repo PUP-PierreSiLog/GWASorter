451b9832b4a0893af395ba14669cf6a9eaec06a6
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-26 07:35:50 +0800

    Fixed error of concatenating float and string by specifying GWA as a float data type

--- GWA_Sorter.py
+++ GWA_Sorter.py
@@ -8,8 +8,10 @@ with open("D:\[CMPE103] OOP\GWA_Sorter\\20 Students") as source:
 #Since I created a CSV, I can now strip and split the name of the students and their GWAs of commas using the function:
 for line in lines:
     name, gwa=line.strip().split(",")
+    gwa=float(gwa)
 #If loop comparing the values of the students' GWA
 if gwa > highest_gwa:
     highest_gwa=gwa
     highest_name=name
 #Print output
+print(highest_name, ", ", highest_gwa)
